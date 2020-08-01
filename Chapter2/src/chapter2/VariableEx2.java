6a137288be49241074827704e7363383e569711e
izonejangwonyoung <2000shim@naver.com>
2020-08-01 22:10:53 +0900

    long형

---
+++ Chapter2/src/chapter2/VariableEx2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class VariableEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		long num = 12345678900L;
+		int level=60;
+		
+	}
+
+}
