e643aad0e97e93f0714de57160746726d6fcbb94
izonejangwonyoung <2000shim@naver.com>
2020-08-01 21:51:27 +0900

    commit 변수와 자료형2 강의 중간

---
+++ Chapter2/src/chapter2/VariableEx.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int num;
+		num=10;
+		
+		System.out.println(num);
+		
+		int level=20;
+		System.out.println(level);
+
+	}
+
+}
