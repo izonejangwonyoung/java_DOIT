3cd2f5ed90f62f08c5cc1ce0244360668a634aa3
izonejangwonyoung <2000shim@naver.com>
2020-08-02 10:15:01 +0900

    ..

--- Chapter2/src/chapter2/Constant.java
+++ Chapter2/src/chapter2/Constant.java
@@ -4,7 +4,18 @@ public class Constant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final int MAX_NUM=100;
+		final float PI=3.12F;
 
+		
+		final int STUDENT_NUM=31;
+		
+		int num=0;
+		if (num==STUDENT_NUM) {}
+		
+		System.out.println(STUDENT_NUM);
+		
+		
 	}
 
 }
