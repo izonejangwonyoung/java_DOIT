6c3a5b752950a43b5747f5ffe80af332beb5a1fd
izonejangwonyoung <2000shim@naver.com>
2020-08-04 18:56:11 +0900

    복합 대입 연산자

---
+++ Chapter3/src/assignment/OperationEx4.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class OperationEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1=10;
+System.out.println(num1 +=1);
+System.out.println(num1 %=10);
+num1 -=3;  //num1=num1-1;
+System.out.println(num1);
+
+	}
+
+}
