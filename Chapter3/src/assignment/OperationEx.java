09b92ba24bbbe37dc088d44ac22cd5c412402bd4
izonejangwonyoung <2000shim@naver.com>
2020-08-04 18:32:55 +0900

    add new chapter

---
+++ Chapter3/src/assignment/OperationEx.java
@@ -0,0 +1,23 @@
+package assignment;
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=24;
+		System.out.println(age);
+		
+		
+		int num=10;
+		int num2=-num;
+		
+		System.out.println(num2);
+		System.out.println(num);
+		
+		num= -num;
+		System.out.println(num);
+		
+
+	}
+
+}
