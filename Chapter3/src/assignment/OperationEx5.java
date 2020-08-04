add09d05b68117ddf5edb80c8b88838dd34bab76
izonejangwonyoung <2000shim@naver.com>
2020-08-04 19:13:57 +0900

    비트 연산자

---
+++ Chapter3/src/assignment/OperationEx5.java
@@ -0,0 +1,30 @@
+package assignment;
+
+public class OperationEx5 {
+
+	public static void main(String[] args) {
+
+//		int  num1=5;
+//		int  num2=10;
+//
+//		
+//		int result=num1&num2;
+//		System.out.println(result);
+//		
+//		int result=num1 | num2;
+//     	System.out.println(result);
+
+		int num3=5;//00000101;
+    	System.out.println(num3<<1);//한 번 미는것
+	    System.out.println(num3);
+    	System.out.println(num3<< 2 );//두 번 미는것
+
+    	System.out.println(num3<< 3 );//세 번 미는것
+    	
+    	System.out.println(num3>> 1 ); //00000010;
+    	
+
+
+	}
+
+}
