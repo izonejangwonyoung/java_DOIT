4f9346d57256bba493bae5da63f23cf2822baae1
izonejangwonyoung <2000shim@naver.com>
2020-08-04 19:01:02 +0900

    조건 연산자

--- Chapter3/src/assignment/OperationEx4.java
+++ Chapter3/src/assignment/OperationEx4.java
@@ -10,6 +10,10 @@ System.out.println(num1 %=10);
 num1 -=3;  //num1=num1-1;
 System.out.println(num1);
 
+
+int num=(5>3)? 10 : 20;   //괄호 안의 식이 참이면 왼쪽, 거짓이면 오른쪽 value 선택해서 println   숫자가 아니라 구문이 와도 그 구문을 수행한다. 응용 가능하다 
+System.out.println(num);
+
 	}
 
 }
