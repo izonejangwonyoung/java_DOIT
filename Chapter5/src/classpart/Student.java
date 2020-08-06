0500c72663da682325e80376d204df5e3c1a0252
izonejangwonyoung <2000shim@naver.com>
2020-08-06 16:11:04 +0900

    class  연습

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -11,14 +11,14 @@ String address;
 public void showStudentInfor() {
 	System.out.println(StudentName+","+address);
 }
-public static void main(String[] args) {
-	
-	Student studentLee=new Student();
-	studentLee.address ="아이즈원";
-	studentLee.StudentName="장원영";
-			
-	studentLee.showStudentInfor(); 
-	}
+//public static void main(String[] args) {
+//	
+//	Student studentLee=new Student();
+//	studentLee.address ="아이즈원";
+//	studentLee.StudentName="장원영";
+//			
+//	studentLee.showStudentInfor(); 
+//	}
 					
  }
 
