dda4ff2c0a27f2808ea5f57f21f906e6724747f6
izonejangwonyoung <2000shim@naver.com>
2020-08-06 17:17:24 +0900

    클래스와 객체 1-2

--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -2,17 +2,28 @@ package classpart;
 
 public class StudentTest {
 	
-	int StudentID;
 
-String StudentName;
-int grade;
-String address;
-
-public void showStudentInfor() {
-	System.out.println(StudentName+","+address);
+public static void main(String[] args) {
+	
+	Student studentLee=new Student();
+	studentLee.StudentName="김채원";
+	studentLee.StudentID=801;
+	studentLee.address="아이즈원";
+	
+	Student studentChoi=new Student();
+	studentChoi.StudentName="최예나";
+	studentChoi.StudentID=627;
+	studentChoi.address="아이즈원";	
+	
+	studentChoi.showStudentInfor();
+	studentLee.showStudentInfor();
+	
+//	System.out.println(studentChoi);
 }
 
- }
+}
+
+ 
 
 	
 
