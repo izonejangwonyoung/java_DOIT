fc3a45d331d6e0c0bb30ba4b2c8c1371a78df243
izonejangwonyoung <2000shim@naver.com>
2020-08-06 16:49:10 +0900

    클래스와 객체 1

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -9,8 +9,34 @@ int grade;
 String address;
 
 public void showStudentInfor() {
-	System.out.println(StudentName+","+address);
+	System.out.println(StudentName+","+address+","+StudentID);
 }
+
+public String getStudentName() {
+	return  StudentName;
+}
+
+public void setStudentName(String name) {
+
+}
+
+public static void main(String[] args) {
+	
+	Student studentLee=new Student();
+	studentLee.StudentName="김채원";
+	studentLee.StudentID=801;
+	studentLee.address="아이즈원";
+	
+	Student studentChoi=new Student();
+	studentChoi.StudentName="최예나";
+	studentChoi.StudentID=627;
+	studentChoi.address="아이즈원";	
+	
+	studentChoi.showStudentInfor();
+	studentLee.showStudentInfor();
+}
+
+
 //public static void main(String[] args) {
 //	
 //	Student studentLee=new Student();
