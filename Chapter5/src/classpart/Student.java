2b862d21542b23aded647c0b91fa16d4610fb639
izonejangwonyoung <2000shim@naver.com>
2020-08-06 17:30:56 +0900

    클래스와 객체 1-2 3차시

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -3,10 +3,17 @@ package classpart;
 public class Student {
 	
 	int StudentID;
+	String StudentName;
+	int grade;
+	String address;
+public Student() {}
+	
+	
+public Student(int id,String name){
+	StudentID=id;
+	StudentName=name;
+}
 
-String StudentName;
-int grade;
-String address;
 
 public void showStudentInfor() {
 	System.out.println(StudentName+","+address+","+StudentID);
@@ -17,24 +24,24 @@ public String getStudentName() {
 }
 
 public void setStudentName(String name) {
-
+	StudentName=name;
 }
 
-public static void main(String[] args) {
-	
-	Student studentLee=new Student();
-	studentLee.StudentName="김채원";
-	studentLee.StudentID=801;
-	studentLee.address="아이즈원";
-	
-	Student studentChoi=new Student();
-	studentChoi.StudentName="최예나";
-	studentChoi.StudentID=627;
-	studentChoi.address="아이즈원";	
-	
-	studentChoi.showStudentInfor();
-	studentLee.showStudentInfor();
-}
+//public static void main(String[] args) {
+//	
+//	Student studentLee=new Student();
+//	studentLee.StudentName="김채원";
+//	studentLee.StudentID=801;
+//	studentLee.address="아이즈원";
+//	
+//	Student studentChoi=new Student();
+//	studentChoi.StudentName="최예나";
+//	studentChoi.StudentID=627;
+//	studentChoi.address="아이즈원";	
+//	
+//	studentChoi.showStudentInfor();
+//	studentLee.showStudentInfor();
+//}
 
 
 //public static void main(String[] args) {
