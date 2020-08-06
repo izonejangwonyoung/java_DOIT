c3f9726b92540d81413c663188e45648b902b9c0
izonejangwonyoung <2000shim@naver.com>
2020-08-06 16:10:51 +0900

    class 연습

---
+++ Chapter5/src/classpart/StudentTest.java
@@ -0,0 +1,19 @@
+package classpart;
+
+public class StudentTest {
+	
+	int StudentID;
+
+String StudentName;
+int grade;
+String address;
+
+public void showStudentInfor() {
+	System.out.println(StudentName+","+address);
+}
+
+ }
+
+	
+
+					
