fb2038f73fa1217a5b74f3a383ce2a167ea4daf0
izonejangwonyoung <2000shim@naver.com>
2020-08-08 16:30:54 +0900

    아이즈원 활용한 practice 패키지 commit

---
+++ Practice/src/izone/NameSaver.java
@@ -0,0 +1,13 @@
+package izone;
+
+public class NameSaver {
+	int name;
+	String gender;
+	int age;
+	int birthday;
+
+
+public void ShowMemberInfo() {
+	System.out.println(name+"는 "+gender+"이며 "+age+"살인 "+birthday+" 출생입니다.");
+	}
+}
