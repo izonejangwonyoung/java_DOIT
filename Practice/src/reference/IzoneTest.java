ba69e4caa728e0a96d48aaee9085a0756c7151bf
izonejangwonyoung <2000shim@naver.com>
2020-08-09 15:02:00 +0900

    일단 commit

---
+++ Practice/src/reference/IzoneTest.java
@@ -0,0 +1,9 @@
+package reference;
+
+public class IzoneTest {
+public static void main(String[] args) {
+	
+	
+	
+	}
+}
--- Practice/src/reference/izone.java
+++ Practice/src/reference/izone.java
@@ -6,5 +6,23 @@ public class izone {
 	int age;
 	String memberName;
 	Grade produce48;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public Grade getProduce48() {
+		return produce48;
+	}
+	public void setProduce48(Grade produce48) {
+		this.produce48 = produce48;
+	}
 	
 }
