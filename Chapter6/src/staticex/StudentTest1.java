f7bb006c5e4770b373d09e1ca8fcd855094261c9
izonejangwonyoung <2000shim@naver.com>
2020-08-09 15:49:11 +0900

    COMMIT NEW package

---
+++ Chapter6/src/staticex/StudentTest1.java
@@ -0,0 +1,19 @@
+package staticex;
+
+public class StudentTest1 {
+
+	public static void main(String[] args) {
+		Student studentjang = new Student();
+		System.out.println(studentjang.serialNum);
+
+		
+		Student studentchoi = new Student();
+		System.out.println(studentchoi.serialNum);
+			
+		
+		
+		System.out.println(studentjang.serialNum);
+		System.out.println(studentchoi.serialNum);
+	}
+
+}
