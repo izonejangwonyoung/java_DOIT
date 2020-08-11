7d95bfb617cf39a978de4360ebba983888c6b803
izonejangwonyoung <2000shim@naver.com>
2020-08-11 20:39:14 +0900

    배열 복사하기

--- Chapter7/src/array/BookArray.java
+++ Chapter7/src/array/BookArray.java
@@ -1,5 +1,8 @@
 package array;
 
+import sun.jvm.hotspot.ui.action.ShowAction;
+
+
 public class BookArray {
 
 	public static void main(String[] args) {
@@ -9,9 +12,16 @@ public class BookArray {
 		for(int i=0; i<library.length; i++){
 			System.out.println(library[i]);
 		}
+
+		library[0]=new Book("태백산맥1","조정래");
+		library[1]=new Book("태백산맥2","조정래");
+		library[2]=new Book("태백산맥3","조정래");
+		library[3]=new Book("태백산맥4","조정래");
+		library[4]=new Book("태백산맥5","조정래");
 		
-		int[] arr=new int[5];
- 
- 
+		for(int i=0; i<library.length; i++){
+			library[i].showBookInfo();
+		}
 	}
 }
+
